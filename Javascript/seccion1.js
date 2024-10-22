32ac8deca4123e7b709f14d6b262ad6d29a84ac8
Gianmario Sosa <sosatrottagianmario@gmail.com>
2024-10-22 09:53:54 -0400

    JS seccion 1 clase 1

---
+++ Javascript/seccion1.js
@@ -0,0 +1,55 @@
+//alert("Me ejecuto desde un archivo exteerno lol");
+//Una linea
+/*
+  varias
+  lineas
+*/
+
+//LA CONSOLA
+console.log("Hola Mundo");
+
+//VARIABLES
+/* Inicializacion de una Variable */
+var edad = 5;
+//Utilizacion de una variable
+console.log(edad);
+
+//TIPOS DE DATOS
+//Numeros
+var numero = 5;
+var numero_float = 7.7;
+//Cadenas de texto string
+var cadena = "Hola mundo";
+//Booleanos
+var booleano = true
+//booleano = false;
+//Arreglos (Arrays)
+var arreglo = [1,2,3,4,5];
+//Objetos
+var objeto = {
+  nombre: "Juan",
+  edad: 25
+};
+console.log(objeto);
+//Indefinido
+var Indefinido;
+//nulo
+var nulo = null;
+
+
+//OPERADORES COMPATIVOS 
+//Aritmeticos
+var suma =  5 + 3;
+var resta =  5 - 3;
+var multiplicar = 5 * 3;
+var dividir = 5 / 3;
+var modulo = 5 % 3;
+var exponente = 5 ** 2;
+
+//Comparacion 
+var igual = 5 == "5";
+var exactamente_igual = 5 === "5";
+var diferente = 5 != 5;
+var exactamente_diferente = 5 !== 5;
+var menor_que = 5 > 3
+var mayor_que = 5 < 3
