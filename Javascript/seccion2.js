6a0ef3117e3da5f84fb63c4cb6f414c475dd6489
Gianmario Sosa <sosatrottagianmario@gmail.com>
2024-11-01 10:53:00 -0400

    CICLOS

--- Javascript/seccion2.js
+++ Javascript/seccion2.js
@@ -837,3 +837,122 @@ function verificarPosicionJuan(nombres) {
 
 const nombres = ["Antonia", "Sebastian", "Juan", "Jose", "Johanny"];
 console.log(verificarPosicionJuan(nombres));
+
+//ciclos (loops)
+
+
+let carros ["toyota" "mercedes" "chevrolet"]
+
+//Ciclos (Loops)
+/*
+  Los ciclos nos permiten repetir un bloque de codigo
+  mientras se cumpla una condicion
+*/
+
+let carros = ["Toyota", "Nissan", "Chevrolet", "Ford"];
+//For
+for(let i = 0; i < carros.length; i++){
+  console.log(carros[i]);
+}
+
+//While
+let i = 0;
+while(i < carros.length){
+  console.log(carros[i]);
+  if(i === 3){
+    break;
+  }
+  i++;
+}
+
+//Do while
+let j = 0;
+do {
+  console.log(carros[j]);
+  j++;
+} while(j < 0);
+
+//For of (Especial para recorrer arrays)
+for(let carro of carros){
+  console.log(carro);
+}
+
+let persona = {
+  nombre: "Juan",
+  apellido: "Perez",
+  edad: 20
+}
+
+//For in (Especial para recorrer objetos)
+for(let propiedad in persona){
+  console.log(persona[propiedad]);
+}
+
+let comidas = [
+  ["Hamburguesa", "Pizza", "Tacos"],
+  ["Sushi", "Ramen", "Gyoza"],
+  ["Pasta", "Lasagna", "Ravioli"]
+];
+
+for(let comida of comidas){
+  for(let platillo of comida){
+    console.log(platillo);
+  }
+}
+
+/*
+  Ejercicios de ciclos
+  1. Crea un ciclo que imprima los numeros del 1 al 100.
+  2. Crea un ciclo que permita imprimir cada uno de los valores de la variable compra.
+  3. Crea un ciclo que imprima los numeros pares del 0 al 100 de dos en dos. (2, 6, 10, 14, 18, ...)
+  4. Realiza el ejercicio de Fibonacci utilizando un ciclo. (Debes imprimir solo los primeros 15 valores)
+*/
+
+//ejercicio 1
+
+for(let i = 0; i <= 100; i++){
+  console.log(i);
+}
+
+//ejercicio 2
+
+let compra = [
+  {
+    producto: "Manzana",
+    precio: 1.5,
+    tipos: ["Golden", "Red", "Green"],
+    cantidad: 10
+  },
+  {
+    producto: "Pera",
+    precio: 2.5,
+    tipos: ["Bartlett", "Anjou", "Bosc"],
+    cantidad: 5
+  }
+]
+
+for(let propiedad in compra){
+  console.log(compra[propiedad]);
+}
+
+//Ejercicio 3
+
+for (let i = 2 ; i <= 100; i = i + 4) {
+  console.log(i);
+}
+
+
+//ejercico 4
+
+function fibonacci(numero) {
+  let a = 0, b = 1, c;
+  for (let i = 0; i < numero; i++) {
+    console.log(a);
+    c = a;
+    a = b;
+    b = c + b;
+  }
+}
+console.log(fibonacci(15));
+
+
